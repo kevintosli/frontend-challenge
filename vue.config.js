e0b19435cb0cb794a85edba2e031c1158d7e40dc
Kevin Tosli <kevintosli@icloud.com>
2019-11-07 16:22:31 -0300

    Added js and routes aliases

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,9 @@ module.exports = {
     resolve: {
       alias: {
         "@assets": path.resolve(__dirname, "src/assets"),
+        "@js": path.resolve(__dirname, "src/js"),
         "@components": path.resolve(__dirname, "src/components"),
+        "@routes": path.resolve(__dirname, "src/routes"),
         "@views": path.resolve(__dirname, "src/views")
       }
     }
